d22975267d62f455201d8cf531a5603d9bbd8ee2
hulkoba <fuchspippi@web.de>
2019-03-03 12:40:40 +0100

    add dummy tasks

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const telegramBot = require('node-telegram-bot-api');
-const env = require('dotenv').config();
+const telegramBot = require('node-telegram-bot-api')
+const cron = require("node-cron")
+const env = require('dotenv').config()
 
-const token = process.env.BOT_TOKEN;
+const tasks = require('./tasks.json')
+const token = process.env.BOT_TOKEN
 
-// const bot = new telegramBot (token, {polling:true});
-let reminder;
+const bot = new telegramBot(token)
+// const bot = new telegramBot(token, { polling:true })
+
+bot.onText(/\/init/,(msg, match)=>{
+  bot.sendMessage(
+    msg.chat.id,
+    `Hey! ${msg.chat.first_name} :wave:`
+  )
+  initCronJobs(msg)
+})
+
+/*
+* second 0-59 | optional
+* minute 0-59
+* hour 0-23
+* dayOfMonth 1-31
+* month 1-12
+* dayOfWeek 0-7
+*/
+const initCronJobs = (message) => {
+  for (const task in tasks) {
+    const pointInTime = tasks[task]
+
+    cron.schedule(pointInTime, () => {
+      bot.sendMessage(
+        message.chat.id,
+        `+++ Erinnerung +++ %0A%0A${task} muss erledigt werden`)
+    })
+  }
+}
