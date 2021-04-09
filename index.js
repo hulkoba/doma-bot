602c9e49314dd1c925cd35b51294f04da47af9a5
hulkoba <jacoba@neighbourhood.ie>
2021-04-09 15:21:48 +0200

    feat: remove pouchdb
    and use nano instead

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const PouchDB = require('pouchdb')
 const telegramBot = require('node-telegram-bot-api')
 require('dotenv').config()
+
+const domasDb = require('nano')(`${process.env.COUCH_URL}/domas`)
 
 const { initTaskJobs, getTaskAnswers } = require('./tasks.js')
 const { checkCalendar } = require('./calendar.js')
 
-const localDb = new PouchDB('domas')
-const remoteDB = new PouchDB(`${process.env.COUCH_URL}/domas`)
-// The remote database will not be created until you do an API call, e.g.: db.info().
-// The reason behind that is that the PouchDB constructor is completely synchronous, for ease of error handling.
-remoteDB.info()
-
-const token = process.env.BOT_TOKEN
-const bot = new telegramBot(token, { polling: true })
+const bot = new telegramBot(process.env.BOT_TOKEN, { polling: true })
 
 bot.onText(/\/start/, (msg, match) => {
-
-  syncDbs()
-
   bot.sendMessage(
     msg.chat.id,
     'Hallo Domas!'
@@ -26,6 +17,7 @@ bot.onText(/\/start/, (msg, match) => {
     // TODO Need to adapt the pointInTime for each task
     // initTaskJobs({ chatId: message.chat.id, bot })
 
+    // This checks the local waste calendar
     checkCalendar({ bot, chatId: msg.chat.id })
 
     // type @DomatoBot
@@ -34,84 +26,74 @@ bot.onText(/\/start/, (msg, match) => {
       show all available tasks
       and show success message on click
       */
-      const results = getTaskAnswers()
-      bot.answerInlineQuery(query.id, results)
+      const taskAnswers = getTaskAnswers()
+      bot.answerInlineQuery(query.id, taskAnswers)
     })
 
     /*
     on selected task -> increase score and update doma-human
     */
-    bot.on('chosen_inline_result', (query) => {
+    bot.on('chosen_inline_result', async query => {
       const [taskName, score] = query.result_id.split('/')
       const id = String(query.from.id)
 
-      // update domaDoc
-      localDb.get(id).then(doc => {
+      let humanDoc
+      try {
+        // get human from db
+        humanDoc = await domasDb.get(id)
+
+        // update tasklist and scores
         const updatedHuman = {
-          ...doc,
-          scores: doc.scores + Number(score),
-          tasks: updatedTasks(doc.tasks, taskName),
+          ...humanDoc,
+          scores: Number(humanDoc.scores) + Number(score),
+          tasks: getUpdatedTasks(humanDoc.tasks, taskName),
           updatedAt: new Date().toISOString()
         }
-        return localDb.put(updatedHuman).then().catch(error => console.log('Could not update human', error))
-      }).catch(error => {
-        // Human does a task for the first time -> create domaDoc
-        if (error.status === 404) {
-          return localDb.put({
+        await domasDb.insert(updatedHuman)
+      } catch (error) {
+        // human does not exist in the database - creates a task for the firt time
+        if (error.statusCode === 404) {
+          const newHuman = {
             _id: id,
             name: query.from.first_name,
             tasks: [taskName],
             scores: score,
             createdAt: new Date().toISOString()
-          }).then().catch(error => console.log('Could not create human', error))
-        } else {
-          console.log('Could not get human', error)
+          }
+          await domasDb.insert(newHuman)
         }
-      })
+        console.log('Could not update human', error)
+      }
     })
 
     /*
     type '/domas' to list all domas with scores
     */
-    bot.onText(/\/domas/,(message, match) => {
+    bot.onText(/\/domas/, async (message, match) => {
       // get all domas
-      localDb.allDocs({
-        include_docs: true
-      }).then(function(result) {
-        const domasList = getListOfDomasWithScores(result)
-
-        bot.sendMessage(message.chat.id, domasList)
-      })
+      const allDomas = await domasDb.list({ include_docs: true })
+      if (allDomas) {
+        const domasAsMarkdownList = getListOfDomasWithScores(allDomas.rows)
+        bot.sendMessage(message.chat.id, domasAsMarkdownList)
+      }
     })
+
   }).catch(error => {
     bot.sendMessage(msg.chat.id, `Möp möp! ${error}`)
   })
 })
 
-const syncDbs = () => {
-  localDb.sync(remoteDB, {
-    live: true,
-    include_docs: true,
-    retry: true
-  })
-  // .on('change', change => console.log(change, 'changed!'))
-  // .on('paused', info => console.log('replication paused.'))
-  // .on('active', info => console.log('replication resumed.', info))
-  // .on('denied', info => console.log('+++ DENIED +++', info))
-  .on('error', err => console.log('+++ ERROR ERROR ERROR +++.', err))
-}
-
-const updatedTasks = (docTasks = [], taskName) => {
-  const updatedTasks = []
-  if(!docTasks.includes(taskName)) {
+const getUpdatedTasks = (tasks = [], taskName) => {
+  const updatedTasks = tasks
+  if (!tasks.includes(taskName)) {
     updatedTasks.push(taskName)
   }
   return updatedTasks
 }
 
-const getListOfDomasWithScores = (result) => {
+const getListOfDomasWithScores = (domas) => {
   let markdownDomas = ''
-  result.rows.forEach(human => {
+  domas.forEach(human => {
     markdownDomas += `${human.doc.name}: ${human.doc.scores} Punkte\n\r`
   })
   return markdownDomas
